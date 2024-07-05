f8aecfbf6afef00b20d68f2039e5328eb50ccf86
RexshackGaming <rexshackuk@gmail.com>
2024-07-05 07:37:10 +0100

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -154,3 +154,11 @@ CREATE TABLE IF NOT EXISTS `favorites_animations` (
   `favorites` longtext DEFAULT ('[]') NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `player_jobs` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `citizenid` varchar(50) DEFAULT NULL,
+    `job` varchar(50) DEFAULT NULL,
+    `grade` INT(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
