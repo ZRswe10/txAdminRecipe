7e932faff2a9ccd5368847f267824e0583c786aa
ZRswe10 <103931424+ZRswe10@users.noreply.github.com>
2024-10-17 20:11:23 +0200

    Update rsgcore.sql

--- rsgcore.sql
+++ rsgcore.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `bans` (
   `license` varchar(50) DEFAULT NULL,
   `discord` varchar(50) DEFAULT NULL,
   `ip` varchar(50) DEFAULT NULL,
-  `reason` text DEFAULT NULL,
+  `reason` text,  -- Removed DEFAULT NULL for TEXT
   `expire` int(11) DEFAULT NULL,
   `bannedby` varchar(255) NOT NULL DEFAULT 'Anticheat',
   PRIMARY KEY (`id`),
@@ -20,13 +20,13 @@ CREATE TABLE IF NOT EXISTS `players` (
   `license` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `outlawstatus` int(11) NOT NULL DEFAULT 0,
-  `money` text NOT NULL,
-  `charinfo` text DEFAULT NULL,
-  `job` text NOT NULL,
-  `gang` text DEFAULT NULL,
-  `position` text NOT NULL,
-  `metadata` text NOT NULL,
-  `inventory` longtext DEFAULT NULL,
+  `money` text NOT NULL, -- Removed DEFAULT for TEXT
+  `charinfo` text, -- Removed DEFAULT NULL for TEXT
+  `job` text NOT NULL, -- Removed DEFAULT for TEXT
+  `gang` text, -- Removed DEFAULT NULL for TEXT
+  `position` text, -- Removed DEFAULT NULL for TEXT
+  `metadata` text NOT NULL, -- Removed DEFAULT for TEXT
+  `inventory` longtext, -- Removed DEFAULT NULL for LONGTEXT
   `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`citizenid`),
   KEY `id` (`id`),
@@ -47,14 +47,14 @@ CREATE TABLE IF NOT EXISTS `playeroutfit` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `citizenid` VARCHAR(50) NOT NULL COLLATE 'utf8mb3_general_ci',
   `name` VARCHAR(255) NOT NULL COLLATE 'utf8mb3_general_ci',
-  `clothes` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+  `clothes` LONGTEXT, -- Removed DEFAULT NULL for LONGTEXT
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `inventories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` varchar(255) NOT NULL,
-  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`items`)),
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin CHECK (json_valid(`items`)), -- Removed DEFAULT NULL for LONGTEXT
   PRIMARY KEY (`identifier`),
   KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `player_horses` (
   `horse` varchar(50) DEFAULT NULL,
   `dirt` int(11) DEFAULT 0,
   `horsexp` int(11) DEFAULT 0,
-  `components` LONGTEXT NOT NULL DEFAULT '{}',
+  `components` LONGTEXT NOT NULL, -- Removed DEFAULT '{}'
   `gender` varchar(11) NOT NULL,
   `wild` varchar(11) DEFAULT NULL,
   `active` tinyint(4) DEFAULT 0,
@@ -91,41 +91,42 @@ CREATE TABLE IF NOT EXISTS `player_horses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `player_weapons` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `serial` varchar(16) NOT NULL,
-    `citizenid` varchar(9) NOT NULL,
-    `components` varchar(4096) NOT NULL DEFAULT '{}',
-    `components_before` varchar(4096) NOT NULL DEFAULT '{}',
-    `ammo` int(3) NOT NULL DEFAULT 0,
-    `ammo_express` int(3) NOT NULL DEFAULT 0,
-    `ammo_express_explosive` int(3) NOT NULL DEFAULT 0,
-    `ammo_high_velocity` int(3) NOT NULL DEFAULT 0,
-    `ammo_split_point` int(3) NOT NULL DEFAULT 0,
-    `ammo_buckshot_incendiary` int(3) NOT NULL DEFAULT 0,
-    `ammo_slug` int(3) NOT NULL DEFAULT 0,
-    `ammo_slug_explosive` int(3) NOT NULL DEFAULT 0,
-    `ammo_tranquilizer` int(3) NOT NULL DEFAULT 0,
-    `ammo_fire` int(3) NOT NULL DEFAULT 0,
-    `ammo_poison` int(3) NOT NULL DEFAULT 0,
-    `ammo_dynamite` int(3) NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`)
+  `id` int NOT NULL AUTO_INCREMENT,
+  `serial` varchar(16) NOT NULL,
+  `citizenid` varchar(9) NOT NULL,
+  `components` varchar(4096) NOT NULL DEFAULT '{}',
+  `components_before` varchar(4096) NOT NULL DEFAULT '{}',
+  `ammo` int(3) NOT NULL DEFAULT 0,
+  `ammo_express` int(3) NOT NULL DEFAULT 0,
+  `ammo_express_explosive` int(3) NOT NULL DEFAULT 0,
+  `ammo_high_velocity` int(3) NOT NULL DEFAULT 0,
+  `ammo_split_point` int(3) NOT NULL DEFAULT 0,
+  `ammo_buckshot_incendiary` int(3) NOT NULL DEFAULT 0,
+  `ammo_slug` int(3) NOT NULL DEFAULT 0,
+  `ammo_slug_explosive` int(3) NOT NULL DEFAULT 0,
+  `ammo_tranquilizer` int(3) NOT NULL DEFAULT 0,
+  `ammo_fire` int(3) NOT NULL DEFAULT 0,
+  `ammo_poison` int(3) NOT NULL DEFAULT 0,
+  `ammo_dynamite` int(3) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `address_book` (
-`id` INT(11) NOT NULL AUTO_INCREMENT,
-`citizenid` VARCHAR(50) NOT NULL,
-`name`  VARCHAR(50) NOT NULL,
-`owner`  VARCHAR(50) NOT NULL,
-PRIMARY KEY (`id`));
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` VARCHAR(50) NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `owner` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 CREATE TABLE IF NOT EXISTS `management_funds` (
-`id` INT(11) NOT NULL AUTO_INCREMENT,
-`job_name` VARCHAR(50) NOT NULL,
-`amount`  INT(100) NOT NULL,
-`type` ENUM('boss','gang') NOT NULL DEFAULT 'boss',
-PRIMARY KEY (`id`),
-UNIQUE KEY `job_name` (`job_name`),
-KEY `type` (`type`)
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `job_name` VARCHAR(50) NOT NULL,
+  `amount` INT(100) NOT NULL,
+  `type` ENUM('boss','gang') NOT NULL DEFAULT 'boss',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `job_name` (`job_name`),
+  KEY `type` (`type`)
 );
 
 INSERT INTO `management_funds` (`job_name`, `amount`, `type`) VALUES
@@ -139,14 +140,14 @@ INSERT INTO `management_funds` (`job_name`, `amount`, `type`) VALUES
 CREATE TABLE IF NOT EXISTS `favorites_animations` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(50) NOT NULL,
-  `favorites` longtext DEFAULT ('[]') NOT NULL,
+  `favorites` longtext NOT NULL, -- Removed DEFAULT ('[]')
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `player_jobs` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `citizenid` varchar(50) DEFAULT NULL,
-    `job` varchar(50) DEFAULT NULL,
-    `grade` INT(11) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(50) DEFAULT NULL,
+  `job` varchar(50) DEFAULT NULL,
+  `grade` INT(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
